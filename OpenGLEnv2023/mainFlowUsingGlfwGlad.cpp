70c46bd6586c1722123368af15e9902f7b43c4b1
Emmanuel.Lo <snowray712000@gmail.com>
2023-04-03 22:21:48 +0800

    v1.1.1_2023.4.3 範例2，旋轉與平移，視窗大小改變

--- OpenGLEnv2023/mainFlowUsingGlfwGlad.cpp
+++ OpenGLEnv2023/mainFlowUsingGlfwGlad.cpp
@@ -5,28 +5,60 @@
 #include "mainFlowUsingGlfwGlad.h"
 
 #include <iostream> // cout
+#include <math.h>
 #include "EasyImGui.h"
 #include "EasyIO.h"
 #include "EasyString.h"
 
+#define M_PI       3.14159265358979323846   // pi
 MainFlowUsingGlfwGlad& MainFlowUsingGlfwGlad::s() {
 	static MainFlowUsingGlfwGlad r1;
 	return r1;
 }
-// callbackKey1
-MainFlowUsingGlfwGlad* pObjectCallbackKey = nullptr;
-void MainFlowUsingGlfwGlad::callbackGlfwKey(GLFWwindow* window, int keycode, int scancode, int action, int mods) {
-	if (pObjectCallbackKey != nullptr) {
-		pObjectCallbackKey->callbackKey(window, keycode, scancode, action, mods);
+void MainFlowUsingGlfwGlad::key_callbacks(GLFWwindow* window, int keycode, int scancode, int action, int mods) {
+	s().key_callback(window, keycode, scancode, action, mods);
+}
+void MainFlowUsingGlfwGlad::mouse_callbacks(GLFWwindow* window, int button, int action, int mods) {
+	s().mouse_callback(window, button, action, mods);
+}
+void MainFlowUsingGlfwGlad::cursor_callbacks(GLFWwindow* window, double xpos, double ypos) {
+	s().cursor_callback(window, xpos, ypos);
+}
+void MainFlowUsingGlfwGlad::framebuffer_size_callbacks(GLFWwindow* window, int width, int height) {
+	s().framebuffer_size_callback(window, width, height);
+}
+void MainFlowUsingGlfwGlad::guiMainMenu() {
+
+	if (ImGui::BeginMainMenuBar()) {
+		if (ImGui::BeginMenu("File", true)) {
+			if (ImGui::MenuItem("Open", "Ctrl+O")) {
+				this->key_callback(window, GLFW_KEY_O, 24, GLFW_PRESS, GLFW_MOD_CONTROL);
+			}
+			ImGui::EndMenu();
+		}
+		ImGui::EndMainMenuBar();
 	}
 }
-
+void MainFlowUsingGlfwGlad::glDrawXYZAxis() {
+	// x-y-z 軸
+	glBegin(GL_LINES);
+	glColor3ub(255, 0, 0);
+	glVertex3f(0, 0, 0);
+	glVertex3f(1, 0, 0);
+	glColor3ub(0, 255, 0);
+	glVertex3f(0, 0, 0);
+	glVertex3f(0, 1, 0);
+	glColor3ub(0, 0, 255);
+	glVertex3f(0, 0, 0);
+	glVertex3f(0, 0, 1);
+	glEnd();
+}
 bool MainFlowUsingGlfwGlad::mainloopPre() {
 	GLFWwindow* window;
 	if (!glfwInit())
 		return false;
 
-	window = glfwCreateWindow(500, 500, "Hello World", NULL, NULL);
+	window = glfwCreateWindow(cxwin, cywin, "Hello World", NULL, NULL);
 
 	if (!window)
 	{
@@ -45,8 +77,10 @@ bool MainFlowUsingGlfwGlad::mainloopPre() {
 
 	EasyImGUI::init(window);
 
-	glfwSetKeyCallback(window, callbackGlfwKey);
-	pObjectCallbackKey = this;
+	glfwSetKeyCallback(window, key_callbacks);
+	glfwSetMouseButtonCallback(window, mouse_callbacks);
+	glfwSetCursorPosCallback(window, cursor_callbacks);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callbacks);
 
 	return true;
 }
@@ -57,13 +91,65 @@ void MainFlowUsingGlfwGlad::mainloopAfter() {
 	glfwTerminate();
 }
 
-void MainFlowUsingGlfwGlad::callbackKey(GLFWwindow* window, int keycode, int scancode, int action, int mods) {
+void MainFlowUsingGlfwGlad::key_callback(GLFWwindow* window, int keycode, int scancode, int action, int mods) {
 	if (keycode == GLFW_KEY_O && mods == GLFW_MOD_CONTROL && action == GLFW_PRESS)
 	{
 		// 當 快速鍵 與 menu 綁定一起時，則在 menu 直接呼叫 callbackKey，參數要知道，就打開 print
 		// printf("%d %d %d %d", keycode, scancode, action, mods); // 79 24 1 2
-		auto path = EasyIO::OpenFileDialogEasyImage();
+		auto path = EasyIO::OpenFileDialogEasy(_T("STL Files\0*.stl\0All Files(*.*)\0*.*\0"));
 		std::cout << EasyString::stringwTostring(path); 
+	}
+}
+
+void MainFlowUsingGlfwGlad::mouse_callback(GLFWwindow* window, int button, int action, int mods) {
+	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+	{
+		isPressedLButton = true;
+		glfwGetCursorPos(window, &xPressedInstant, &yPressedInstant);
+		xPressedLast = xPressedInstant;
+		yPressedLast = yPressedInstant;
+		
+		ImGui::GetIO().MouseDown[0] = true; // 沒有的話，會使 ImGUI 的 Menu 按了卻無反應。
+	}
+	else if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE)
+	{
+		isPressedLButton = false;
+		
+		ImGui::GetIO().MouseDown[0] = false;// 沒有的話，會使 ImGUI 的 Menu 按了卻無反應。
+	}
+}
+
+void MainFlowUsingGlfwGlad::cursor_callback(GLFWwindow* window, double xpos, double ypos) {
+	ImGui::GetIO().MousePos = ImVec2(xpos, ypos);  // // 沒有的話，會使 ImGUI 的 Menu 按了卻無反應。 通知 ImGui 鼠標位置的變化
+	if (isPressedLButton)
+	{
+		double deltaX = xpos - xPressedLast;
+		double deltaY = ypos - yPressedLast;
+		angleX += deltaY / cxwin * M_PI;
+		angleY += deltaX / cywin * M_PI;
+		xPressedLast = xpos;
+		yPressedLast = ypos;
+	}
+}
+
+void MainFlowUsingGlfwGlad::framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	cxwin = width;
+	cywin = height;
+
+	glViewport(0, 0, width, height);  // 設置 OpenGL 視口大小為窗口大小
+
+	glMatrixMode(GL_PROJECTION);  // 切換到投影矩陣模式
+	glLoadIdentity();  // 重置矩陣
+
+	// 計算新的投影矩陣，保持長寬比不變
+	double aspect_ratio = (double)width / height;
+	if (aspect_ratio > 1)
+	{
+		glOrtho(-aspect_ratio, aspect_ratio, -1, 1, 1, -1);
+	}
+	else
+	{
+		glOrtho(-1, 1, -1 / aspect_ratio, 1 / aspect_ratio, 1, -1);
 	}
 }
 
@@ -73,18 +159,16 @@ void MainFlowUsingGlfwGlad::mainLoop() {
 	{
 		EasyImGUI::callThisBeforeGLClear();
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		if (ImGui::BeginMainMenuBar()) {
-			if (ImGui::BeginMenu("File", true)) {
-				if (ImGui::MenuItem("Open", "Ctrl+O")) {
-					this->callbackKey(window, GLFW_KEY_O, 24, GLFW_PRESS, GLFW_MOD_CONTROL);
-				}
-				ImGui::EndMenu();
-			}
-			ImGui::EndMainMenuBar();
-		}
-
+		this->guiMainMenu();
 		EasyImGUI::callThisAfterGLClearBeforeSwapBuffer();
 
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
+		glRotatef(angleX * 180 / M_PI, 1, 0, 0);
+		glRotatef(angleY * 180 / M_PI, 0, 1, 0);
+		this->glDrawXYZAxis();
+		
+		glColor3ub(255, 255, 255);
 		glBegin(GL_TRIANGLES);
 		glVertex2f(0, 0);
 		glVertex2f(0.5, 0);
--- OpenGLEnv2023/mainFlowUsingGlfwGlad.h
+++ OpenGLEnv2023/mainFlowUsingGlfwGlad.h
@@ -1,6 +1,13 @@
 #pragma once
 #include <GLFW/glfw3.h>
 class MainFlowUsingGlfwGlad {
+    bool isPressedLButton = false;
+    double xPressedLast, yPressedLast;
+    double xPressedInstant, yPressedInstant;
+private:
+    double angleX = 0, angleY = 0; // openGL rotate x rotate y
+private:
+    int cxwin = 500, cywin = 500; 
 public:
 	GLFWwindow* window;
     static MainFlowUsingGlfwGlad& s();
@@ -10,6 +17,16 @@ protected:
 public:
     void mainLoop();
 private:
-    void callbackKey(GLFWwindow* window, int keycode, int scancode, int action, int mods);
-    static void callbackGlfwKey(GLFWwindow* window, int keycode, int scancode, int action, int mods);
+    void key_callback(GLFWwindow* window, int keycode, int scancode, int action, int mods);
+    static void key_callbacks(GLFWwindow* window, int keycode, int scancode, int action, int mods);
+private:
+    void mouse_callback(GLFWwindow* window, int button, int action, int mods);
+    void cursor_callback(GLFWwindow* window, double xpos, double ypos);
+    void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+    static void mouse_callbacks(GLFWwindow* window, int button, int action, int mods);
+    static void cursor_callbacks(GLFWwindow* window, double xpos, double ypos);
+    static void framebuffer_size_callbacks(GLFWwindow* window, int width, int height);
+private:
+    void guiMainMenu();
+    void glDrawXYZAxis();// x-y-z ¶b
 };
